c17e1f3dd060fed8d64ac47989038ab683108598
javierbergmann <javier.bergmann@gmail.com>
2022-05-13 20:35:00 -0300

    grabar

--- js/js.js
+++ js/js.js
@@ -1,2 +1,19 @@
-``
-``
+//vamos a declarar las variables para trabajar con el menu responsivle
+let btnOpenMenu= document.getElementById("btnOpenMenu"); //boton para abrir menu responsive
+let btnCloseMenu= document.getElementById("btnCloseMenu"); //boton que cierra el menu responsive
+let menuResponsive= document.getElementById("menuBar"); //menu responsive que debemos abrir
+let enlaces= document.getElementById("enlaces"); //cuando se presiona algunos de los enlaces tambien debe 
+                                                    //cerrarsede manera suave
+
+btnOpenMenu.addEventListener("click", ()=>{
+    menuResponsive.className="menu__bar active";
+})
+
+btnCloseMenu.addEventListener("click", ()=>{
+    menuResponsive.className="menu__bar";
+})
+
+enlaces.addEventListener("click", function(){
+    menuResponsive.style.transitionDelay= "0.5s";
+    menuResponsive.className= "menu__bar";
+})
